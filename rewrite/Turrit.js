d165f43fa924f5244fbc9da57930d4036a1637ee
duoxiong <156549891+duoxiong@users.noreply.github.com>
2025-03-24 15:02:53 +0800

    Turrit.js

--- rewrite/Turrit.js
+++ rewrite/Turrit.js
@@ -1,7 +1,7 @@
 [rewrite_local]
 # Telegram 跳转至 Turrit
-^https?://(www\.)?t\.me/(.*) url 302 https://turrit.xyz/$2
-^https?://(www\.)?telegram\.org/(.*) url 302 https://turrit.xyz/$2
+^https?://(www\.)?t\.me/(.*) url 301 https://turrit.xyz/$2
+^https?://(www\.)?telegram\.org/(.*) url 301 https://turrit.xyz/$2
 
 [mitm]
 hostname = t.me, telegram.org
